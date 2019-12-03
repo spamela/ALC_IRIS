9328e0310c0034111c32197cb96a518196161cc2
Stan <stanislas.pamela@hotmail.com>
2019-12-03 08:38:35 +0000

    adding example input i/o script

---
+++ Dakota/web_front/hello_container/input_output_example/convert_input.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# --- This is an example bash script that uses a .csv file to change a given code input file
+
+# --- Check input arguments (need 3 names...)
+if [ "$#" -ne "3" ]
+then
+  echo "usage: "
+  echo "./convert_input.sh <input_file.template> <input_values.cvs> <input_file.new>"
+fi
+
+# --- In our case, we know the csv file only has one line with 3 values, get them...
+my_var=()
+csv_values=`tail -n 1 $2`
+IFS=','
+read -ra ADDR <<< "$csv_values"
+for i in "${ADDR[@]}"
+do
+  my_var+=($i)
+done
+
+# --- Now just use sed to replace the values in our input file
+cp $1 tmp.input
+sed "s/my_first_variable = 1.2345/my_first_variable = ${my_var[0]}/g" tmp.input > tmp.output
+mv tmp.output tmp.input
+sed "s/<xml>0.9876<\/xml>/<xml>${my_var[1]}<\/xml>/g" tmp.input > tmp.output
+mv tmp.output tmp.input
+sed "s/\"var2\": \"2.345\"/\"var2\": \"${my_var[2]}\"/g" tmp.input > tmp.output
+mv tmp.output $3
+rm tmp.input
+
