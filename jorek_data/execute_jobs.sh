1a20da462cb7340b5f87b90eef8794840b6b0c3a
Stan Pamela <stanislas.pamela@hotmail.com>
2021-01-12 14:59:36 +0000

    changing jobs to model 500

--- jorek_data/execute_jobs.sh
+++ jorek_data/execute_jobs.sh
@@ -14,10 +14,10 @@ done
 
 # --- Run JOREK and data-extractor
 export OMP_NUM_THREADS=$n_cpu
-mpirun -np 1 --allow-run-as-root ./jorek_model303 < ./inmast_grid > ./jorek_output_grid.txt
-mpirun -np 1 --allow-run-as-root ./jorek_model303 < ./inmast > ./jorek_output_equi.txt
-mpirun -np $n_cpu --allow-run-as-root ./fortran_process -jorek_file jorek00001.h5 -save_pixels -resolution 0.05 -3D_grid > ./jorek_output_data_grid.txt
-mpirun -np $n_cpu --allow-run-as-root ./fortran_process -jorek_file jorek00001.h5 -use_pixel_file -variable custom -bin_data -3D_grid > ./jorek_output_data.txt
+mpirun -np 1 --allow-run-as-root ./jorek_model500 < ./inmast_grid > ./jorek_output_grid.txt
+mpirun -np 1 --allow-run-as-root ./jorek_model500 < ./inmast > ./jorek_output_equi.txt
+mpirun -np $n_cpu --allow-run-as-root ./fortran_process -jorek_file jorek00001.h5 -save_pixels -resolution 0.02 -3D_grid > ./jorek_output_data_grid.txt
+mpirun -np $n_cpu --allow-run-as-root ./fortran_process -jorek_file jorek00001.h5 -use_pixel_file -variable D_alpha -bin_data -3D_grid > ./jorek_output_data.txt
 
 
 exit 0
