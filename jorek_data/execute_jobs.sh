41c4418256649bd6d8461e91d5eeea1919abb0b5
Stan Pamela <stanislas.pamela@hotmail.com>
2021-01-13 10:02:31 +0000

    printing more variables

--- jorek_data/execute_jobs.sh
+++ jorek_data/execute_jobs.sh
@@ -17,7 +17,7 @@ export OMP_NUM_THREADS=$n_cpu
 mpirun -np 1 --allow-run-as-root ./jorek_model500 < ./inmast_grid > ./jorek_output_grid.txt
 mpirun -np 1 --allow-run-as-root ./jorek_model500 < ./inmast > ./jorek_output_equi.txt
 mpirun -np $n_cpu --allow-run-as-root ./fortran_process -jorek_file jorek00001.h5 -save_pixels -resolution 0.02 -3D_grid > ./jorek_output_data_grid.txt
-mpirun -np $n_cpu --allow-run-as-root ./fortran_process -jorek_file jorek00001.h5 -use_pixel_file -variable D_alpha -bin_data -3D_grid > ./jorek_output_data.txt
+mpirun -np $n_cpu --allow-run-as-root ./fortran_process -jorek_file jorek00001.h5 -use_pixel_file -variable D_alpha -variable BR -variable BZ -variable Bp -variable rho -variable rho_n -variable T -bin_data -3D_grid > ./jorek_output_data.txt
 
 
 exit 0
