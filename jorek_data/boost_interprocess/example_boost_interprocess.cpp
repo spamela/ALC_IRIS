a74716246136850c5ca00a5144e9aad0a9647742
Stan Pamela <stanislas.pamela@hotmail.com>
2021-01-16 00:37:25 +0000

    Now able to share binary file content in shared memory

--- jorek_data/boost_interprocess/example_boost_interprocess.cpp
+++ jorek_data/boost_interprocess/example_boost_interprocess.cpp
@@ -2,6 +2,8 @@
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/allocators/allocator.hpp>
 #include <boost/interprocess/containers/string.hpp>
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
 //std libraries
 #include <cstdlib> //std::system
 #include <cstddef>
@@ -21,56 +23,41 @@ int main(int argc, char *argv[])
   typedef allocator<char, managed_shared_memory::segment_manager> CharAllocator;
   typedef basic_string<char, std::char_traits<char>, CharAllocator> string;
 
+
+  //Input file (note: "binary" flag needs to be consistent with tile)
+  //char input_filename[]  = "myfile.txt";
+  char input_filename[]  = "myfile_short.bin";
+  //Output file
+  char output_filename[] = "output.file.tmp";
+
+
   printf("\n");
 
-  //Parent process
+  //Parent process (allocates memory, and then sleeps while other processes can be launched)
   if(argc == 1)
   {
-    //File reading variables
-    FILE * pFile;
-    long lSize;
-    char *buffer;
-    size_t result;
-
-    //Read a text file
-    printf("Reading txt file...\n");
-    pFile = fopen ( "myfile.txt" , "r" );
-    if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
-    //Get file size:
-    fseek (pFile , 0 , SEEK_END);
-    lSize = ftell (pFile);
-    rewind (pFile);
-    //Allocate memory to contain the whole file:
-    buffer = (char*) malloc (sizeof(char)*lSize);
-    if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
-    //Copy the file into the buffer:
-    result = fread (buffer,1,lSize,pFile);
-    if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
-    //Close file
-    fclose (pFile);
-
-    //Read a binary file
-    printf("Reading unformatted file...\n");
-    std::ifstream file_stream("myfile.txt");
-    file_stream.seekg(0, std::ios::end);
-    size_t size = file_stream.tellg();
-    std::string buffer_sbin(size, ' ');
-    file_stream.seekg(0);
-    file_stream.read(&buffer_sbin[0], size); 
-    std::cout << "byte 0 in file: " << buffer_sbin[0] << "\n";
-
-    //Remove shared memory on construction and destruction
-    struct shm_remove
+    //We have a first shared memory map for various variables
+    struct shm_remove //Remove shared memory on construction and destruction
     {
       shm_remove() { shared_memory_object::remove("Boost_shared_memory"); }
       ~shm_remove(){ shared_memory_object::remove("Boost_shared_memory"); }
     } remover;
-
     //Construct managed shared memory
-    managed_shared_memory managed_shm{open_or_create, "Boost_shared_memory", 10000};
+    managed_shared_memory managed_shm{open_or_create, "Boost_shared_memory", 1000000};
 
+    //We have a second shared memory map for a binary file content
+    struct file_shm_remove //Remove shared memory on construction and destruction 
+    {
+       file_shm_remove() { shared_memory_object::remove("Boost_shared_file"); }
+       ~file_shm_remove(){ shared_memory_object::remove("Boost_shared_file"); }
+    } remover_file;
+    //Construct shared memory object
+    shared_memory_object shdmem{open_or_create, "Boost_shared_file", read_write};
+
+    //Now try to allocate the memory
     try
     {
+
       //Just an integer
       int *shared_i = managed_shm.construct<int>("Integer")(99);
       //An array of identical integers (I don't understand how this can ever be useful...)
@@ -80,18 +67,40 @@ int main(int argc, char *argv[])
       int *shared_k = managed_shm.construct_it<int>("Integer_array")[3](&int_initializer[0]);
       //A string
       string *shared_s = managed_shm.construct<string>("String")("Hello!", managed_shm.get_segment_manager());
-      //A string from file
-      //string *shared_fs = managed_shm.construct<string>("FileString")(buffer, managed_shm.get_segment_manager());
-      string *shared_fs = managed_shm.construct<string>("FileString")(buffer_sbin.c_str(), managed_shm.get_segment_manager());
+
+      //Now read a file...
+      FILE * pFile;
+      long file_size;
+      size_t result;
+      //Read a text/binary file
+      printf("Reading file \"%s\" into shared memory...\n",input_filename);
+      pFile = fopen ( input_filename, "rb" );
+      if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
+      //Get file size:
+      fseek (pFile , 0 , SEEK_END);
+      file_size = ftell (pFile);
+      rewind (pFile);
+      //Allocate shared memory object to contain the file exactly (this is very important, not less, not more!)
+      shdmem.truncate(sizeof(char)*file_size);
+      //Map this memory region
+      mapped_region region{shdmem, read_write};
+      //Create character buffer with pointer to the address of mapped region
+      char *buffer = static_cast<char*>(region.get_address());
+      //Copy the file into the buffer
+      result = fread (buffer,1,file_size,pFile);
+      if (result != file_size) {fputs ("Reading error",stderr); exit (3);}
+      //Close file
+      fclose (pFile);
+      //Finally, we used the other shared memory to communicate the size of the file (needed for write file out...)      
+      long *shared_file_size = managed_shm.construct<long>("file_size")(file_size);
+
     }catch (boost::interprocess::bad_alloc &ex)
     {
       printf("Allocation error for shared memory: %s\n",ex.what());
     }
 
-    //Free file buffer
-    free (buffer);
-
     //Sleep a little while memory is kept in background for child processes
+    printf("\n");
     int n_sleep = 6;
     printf("Memory allocated, process sleeping for [%d * 10 seconds]\n",n_sleep);
     for(int i=0; i<n_sleep ; i++)
@@ -100,11 +109,13 @@ int main(int argc, char *argv[])
       sleep(10);
     }
 
+  //Child processes (need dummy argument after executable)
   }else
   {
     //Open managed shared memory
     managed_shared_memory managed_shm{open_only, "Boost_shared_memory"};
 
+    printf("These are the shared data you have retrieved:\n");
     //Find integer
     std::pair<int*, std::size_t> shared_i = managed_shm.find<int>("Integer");
     if (shared_i.first)
@@ -115,7 +126,7 @@ int main(int argc, char *argv[])
     {
       printf("Integer identical array of size %ld found: %d %d\n",shared_j.second,shared_j.first[0],shared_j.first[1]);
     }
-    //Find integer identical array
+    //Find integer array
     std::pair<int*, std::size_t> shared_k = managed_shm.find<int>("Integer_array");
     if (shared_k.first)
     {
@@ -125,14 +136,27 @@ int main(int argc, char *argv[])
     std::pair<string*, std::size_t> shared_s = managed_shm.find<string>("String");
     if (shared_s.first)
     {
-      std::cout << "String found:"  << *shared_s.first << "\n";
+      std::cout << "String found: "  << *shared_s.first << "\n";
     }
-    //Find string from file
-    std::pair<string*, std::size_t> shared_fs = managed_shm.find<string>("FileString");
-    if (shared_fs.first)
-    {
-      std::cout << "File string found:"  << *shared_fs.first << "\n";
-    }
+
+    printf("\n");
+    //Get the file from shared memory object
+    shared_memory_object shdmem{open_only, "Boost_shared_file", read_write};
+    //Map shared memory
+    mapped_region region2{shdmem, read_only};
+    //Create character buffer with pointer to the address of mapped region
+    char *buffer_read = static_cast<char*>(region2.get_address());
+    printf("Retrieved file content from shared memory\n");
+    //Get the size of the file buffer
+    std::pair<long*, std::size_t> shared_file_size = managed_shm.find<long>("file_size");
+    long buf_size = *shared_file_size.first;
+    //Finally, write data back into another file
+    printf("Writing file back out as \"%s\"...\n", output_filename);
+    FILE * pFile;
+    pFile = fopen (output_filename, "wb");
+    fwrite (buffer_read , 1, buf_size, pFile);
+    fclose (pFile);
+
   }
   return 0;
 }
