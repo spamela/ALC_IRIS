61c61a21c93568930567c73f77c3b18a76e126cc
Stan Pamela <stanislas.pamela@hotmail.com>
2021-01-16 14:08:27 +0000

    Now reading file several time to use a lot of memory

--- jorek_data/boost_interprocess/example_boost_interprocess.cpp
+++ jorek_data/boost_interprocess/example_boost_interprocess.cpp
@@ -14,6 +14,9 @@
 #include <fstream>
 #include <iterator>
 #include <vector>
+#include <string>
+//timing
+#include <chrono>
 
 
 int main(int argc, char *argv[])
@@ -30,6 +33,8 @@ int main(int argc, char *argv[])
   //Output file
   char output_filename[] = "output.file.tmp";
 
+  //To test large amounts of memory, we read/map a file many times
+  int n_copies = 3;
 
   printf("\n");
 
@@ -45,15 +50,6 @@ int main(int argc, char *argv[])
     //Construct managed shared memory
     managed_shared_memory managed_shm{open_or_create, "Boost_shared_memory", 1000000};
 
-    //We have a second shared memory map for a binary file content
-    struct file_shm_remove //Remove shared memory on construction and destruction 
-    {
-       file_shm_remove() { shared_memory_object::remove("Boost_shared_file"); }
-       ~file_shm_remove(){ shared_memory_object::remove("Boost_shared_file"); }
-    } remover_file;
-    //Construct shared memory object
-    shared_memory_object shdmem{open_or_create, "Boost_shared_file", read_write};
-
     //Now try to allocate the memory
     try
     {
@@ -68,31 +64,52 @@ int main(int argc, char *argv[])
       //A string
       string *shared_s = managed_shm.construct<string>("String")("Hello!", managed_shm.get_segment_manager());
 
-      //Now read a file...
-      FILE * pFile;
-      long file_size;
-      size_t result;
-      //Read a text/binary file
-      printf("Reading file \"%s\" into shared memory...\n",input_filename);
-      pFile = fopen ( input_filename, "rb" );
-      if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
-      //Get file size:
-      fseek (pFile , 0 , SEEK_END);
-      file_size = ftell (pFile);
-      rewind (pFile);
-      //Allocate shared memory object to contain the file exactly (this is very important, not less, not more!)
-      shdmem.truncate(sizeof(char)*file_size);
-      //Map this memory region
-      mapped_region region{shdmem, read_write};
-      //Create character buffer with pointer to the address of mapped region
-      char *buffer = static_cast<char*>(region.get_address());
-      //Copy the file into the buffer
-      result = fread (buffer,1,file_size,pFile);
-      if (result != file_size) {fputs ("Reading error",stderr); exit (3);}
-      //Close file
-      fclose (pFile);
-      //Finally, we used the other shared memory to communicate the size of the file (needed for write file out...)      
-      long *shared_file_size = managed_shm.construct<long>("file_size")(file_size);
+      // Now read file many times and create new memory map for each one
+      auto start = std::chrono::high_resolution_clock::now();
+      for (int i_copies=0;i_copies<n_copies;i_copies++)
+      {
+        printf("Allocating memory for file copy # %d\n",i_copies);
+        std::string s_copies = std::to_string(i_copies);
+        //Create memory-name and file-length name for each copy
+        std::string shared_memory_name_tmp = "Boost_shared_file" + s_copies;
+        const char *shared_memory_name = shared_memory_name_tmp.c_str();
+        std::string shared_file_length_tmp = "file_size" + s_copies;
+        const char *shared_file_length = shared_file_length_tmp.c_str();
+        //Construct shared memory object
+        shared_memory_object shdmem{open_or_create, shared_memory_name, read_write};
+
+        //Now read a file...
+        FILE * pFile;
+        long file_size;
+        size_t result;
+        //Read a text/binary file
+        printf("Reading file \"%s\" into shared memory space \"%s\"\n",input_filename,shared_memory_name);
+        pFile = fopen ( input_filename, "rb" );
+        if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
+        //Get file size:
+        fseek (pFile , 0 , SEEK_END);
+        file_size = ftell (pFile);
+        rewind (pFile);
+        //Allocate shared memory object to contain the file exactly (this is very important, not less, not more!)
+        shdmem.truncate(sizeof(char)*file_size);
+        //Map this memory region
+        mapped_region region{shdmem, read_write};
+        //Create character buffer with pointer to the address of mapped region
+        char *buffer = static_cast<char*>(region.get_address());
+        //Copy the file into the buffer
+        result = fread (buffer,1,file_size,pFile);
+        if (result != file_size) {fputs ("Reading error",stderr); exit (3);}
+        //Close file
+        fclose (pFile);
+        //Finally, we used the other shared memory to communicate the size of the file (needed for write file out...)      
+        long *shared_file_size = managed_shm.construct<long>(shared_file_length)(file_size);
+      }
+      auto finish = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double> elapsed = finish - start;
+      printf("\n");
+      printf("************************************************\n");
+      printf("Elapsed time reading file %d times: %f\n",n_copies,elapsed.count());
+      printf("************************************************\n");
 
     }catch (boost::interprocess::bad_alloc &ex)
     {
@@ -139,23 +156,50 @@ int main(int argc, char *argv[])
       std::cout << "String found: "  << *shared_s.first << "\n";
     }
 
+    // Now read file many times and create new memory map for each one
     printf("\n");
-    //Get the file from shared memory object
-    shared_memory_object shdmem{open_only, "Boost_shared_file", read_write};
-    //Map shared memory
-    mapped_region region2{shdmem, read_only};
-    //Create character buffer with pointer to the address of mapped region
-    char *buffer_read = static_cast<char*>(region2.get_address());
-    printf("Retrieved file content from shared memory\n");
-    //Get the size of the file buffer
-    std::pair<long*, std::size_t> shared_file_size = managed_shm.find<long>("file_size");
-    long buf_size = *shared_file_size.first;
-    //Finally, write data back into another file
-    printf("Writing file back out as \"%s\"...\n", output_filename);
-    FILE * pFile;
-    pFile = fopen (output_filename, "wb");
-    fwrite (buffer_read , 1, buf_size, pFile);
-    fclose (pFile);
+    double elapsed = 0.0;
+    for (int i_copies=0;i_copies<n_copies;i_copies++)
+    {
+      auto start = std::chrono::high_resolution_clock::now();
+      printf("Mapping memory for file copy # %d\n",i_copies);
+      std::string s_copies = std::to_string(i_copies);
+      //Create memory-name and file-length name for each copy
+      std::string shared_memory_name_tmp = "Boost_shared_file" + s_copies;
+      const char *shared_memory_name = shared_memory_name_tmp.c_str();
+      //Load the shared memory object
+      shared_memory_object shdmem{open_only, shared_memory_name, read_write};
+      //Map shared memory
+      mapped_region region2{shdmem, read_only};
+      //Create character buffer with pointer to the address of mapped region
+      char *buffer_read = static_cast<char*>(region2.get_address());
+      printf("Retrieved file \"%s\" from shared memory space \"%s\"\n",input_filename,shared_memory_name);
+      //timing only for file assimilation, not for file writes...
+      auto finish = std::chrono::high_resolution_clock::now();
+      std::chrono::duration<double> elapsed_tmp = finish - start;
+      elapsed = elapsed + elapsed_tmp.count();
+      //We write a file copy only for the first and last copy (just for verification)
+      if ( (i_copies == 0) || (i_copies == n_copies-1) )
+      {
+        //Get the size of the file buffer
+        std::string shared_file_length_tmp = "file_size" + s_copies;
+        const char *shared_file_length = shared_file_length_tmp.c_str();
+        std::pair<long*, std::size_t> shared_file_size = managed_shm.find<long>(shared_file_length);
+        long buf_size = *shared_file_size.first;
+        //Finally, write data back into another file
+        std::string output_filename_tmp_tmp = output_filename + s_copies;
+        const char *output_filename_tmp = output_filename_tmp_tmp.c_str();
+        printf("Writing file back out as \"%s\"...\n", output_filename_tmp);
+        FILE * pFile;
+        pFile = fopen (output_filename_tmp, "wb");
+        fwrite (buffer_read , 1, buf_size, pFile);
+        fclose (pFile);
+      }
+    }
+    printf("\n");
+    printf("************************************************\n");
+    printf("Elapsed time retrieving file %d times: %f\n",n_copies,elapsed);
+    printf("************************************************\n");
 
   }
   return 0;
